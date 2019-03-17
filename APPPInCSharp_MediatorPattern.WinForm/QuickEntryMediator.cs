6b27b08145cc723e8fad41c6422ecef916c2815c
Kubing <t6m8@hotmail.com>
2019-03-17 17:28:08 +0800

    init commit

---
+++ APPPInCSharp_MediatorPattern.WinForm/QuickEntryMediator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace APPPInCSharp_MediatorPattern.WinForm
+{
+    public class QuickEntryMediator
+    {
+        private TextBox itsTextBox;
+        private ListBox itsList;
+
+        public QuickEntryMediator(TextBox t, ListBox l)
+        {
+            itsTextBox = t;
+            itsList = l;
+            itsTextBox.TextChanged += new EventHandler(TextFieldChanged);
+        }
+
+        private void TextFieldChanged(object source, EventArgs args)
+        {
+            string prefix = itsTextBox.Text;
+
+            if (prefix.Length == 0)
+            {
+                itsList.ClearSelected();
+                return;
+            }
+
+            ListBox.ObjectCollection listItems = itsList.Items;
+            bool found = false;
+            for (int i = 0; found == false && i < listItems.Count; i++)
+            {
+                object o = listItems[i];
+                string s = o.ToString();
+                if (s.StartsWith(prefix))
+                {
+                    itsList.SetSelected(i, true);
+                    found = true;
+                }
+            }
+
+            if (!found)
+            {
+                itsList.ClearSelected();
+            }
+        }
+    }
+}
